6f9989b8a6ac1cd1b89af969875ea4a18da5491a
srajwadi <sunny@leafnode.io>
2015-07-15 11:58:39 +0530

    Optimize imports.

--- src/main/java/backend/service/AppProperties.java
+++ src/main/java/backend/service/AppProperties.java
@@ -3,11 +3,8 @@ package backend.service;
 import backend.ApplicationSetup;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.NestedConfigurationProperty;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 
